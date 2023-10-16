d37e35459c11cfd03eb9e1ee70bbf75cb02966dc
AbashAnewAunty28 <fuwarinn.ponpon@gmail.com>
2023-10-17 02:49:01 +0900

    コメント修正　不要な処理削除　コード整形

--- lib/models/repositories/user_repository.dart
+++ lib/models/repositories/user_repository.dart
@@ -2,32 +2,36 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final authControllerProvider = StateNotifierProvider<AuthController, User?>(
-      (ref) => AuthController(initialUser: FirebaseAuth.instance.currentUser),
+  (ref) => AuthController(initialUser: FirebaseAuth.instance.currentUser),
 );
 
 class AuthController extends StateNotifier<User?> {
-  // ----- Constructor ----- //
   AuthController({User? initialUser}) : super(initialUser) {
     // Userの変更を検知して状態を更新
     _auth.idTokenChanges().listen((user) {
       state = user;
-      if(state != null && state!.emailVerified){
-        print("メールアドレスが認証されました");
-      }
     });
   }
 
   final _auth = FirebaseAuth.instance;
 
-  /// FirebaseAuthの匿名認証でサインインする
-  Future<void> signUp({required String email, required String password,}) async {
-    final userCredential = await _auth.createUserWithEmailAndPassword(email: email, password: password);
+  /// アカウント作成
+  Future<void> signUp({
+    required String email,
+    required String password,
+  }) async {
+    final userCredential = await _auth.createUserWithEmailAndPassword(
+        email: email, password: password);
     state = userCredential.user;
   }
 
-  /// FirebaseAuthの匿名認証でサインインする
-  Future<void> signIn({required String email, required String password,}) async {
-    final userCredential = await _auth.signInWithEmailAndPassword(email: email, password: password);
+  /// ログイン
+  Future<void> signIn({
+    required String email,
+    required String password,
+  }) async {
+    final userCredential = await _auth.signInWithEmailAndPassword(
+        email: email, password: password);
     state = userCredential.user;
   }
 
