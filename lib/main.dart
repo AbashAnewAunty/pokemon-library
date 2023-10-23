39e64a441da2d8c701ce9417c95fa79efe08fcfb
AbashAnewAunty28 <fuwarinn.ponpon@gmail.com>
2023-10-24 00:02:03 +0900

    webでsharedprefが動作しない件修正

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,16 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pokemon_library/base_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'firebase_options.dart';
 
 void main() async {
+  /// webでsharedPrefが動作しないバグ対応
+  /// https://cloud.tencent.com/developer/article/1870603
+  /// おそらく本来はこちらで対処すべき？
+  /// https://stackoverflow.com/questions/56417667/how-to-save-to-web-local-storage-in-flutter-web
+  SharedPreferences.setMockInitialValues({});
 
   /// init firebase
   WidgetsFlutterBinding.ensureInitialized();
